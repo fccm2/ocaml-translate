d5881954269e0eb20b468fc48ff77253fbf79186
Florent Monnier <monnier.florent@gmail.com>
2026-06-17 21:14:11 +0200

    read-the-sentence-from-a-file

--- src/translate.ml
+++ src/translate.ml
@@ -1,3 +1,17 @@
+
+let input_char_opt ic =
+  try Some(input_char ic)
+  with End_of_file -> None
+
+let input_file filename =
+  let ic = open_in filename in
+  let rec aux cc =
+    match input_char_opt ic with
+    | Some c -> aux (c::cc)
+    | None -> close_in ic; String.concat "" (List.map (String.make 1) (List.rev cc))
+  in
+  aux []
+
 let split_langs s =
   if String.contains s '-' then
   begin
@@ -7,19 +21,48 @@ let split_langs s =
   end
   else failwith "lang-split"
 
+let txt_to_sent txt_in =
+  let n = String.length txt_in in
+  let rec aux c cc_wrd cc_wrds =
+    if c >= n then
+    begin
+      if cc_wrd <> []
+      then
+        let word = String.concat "" (List.map (String.make 1) (List.rev cc_wrd)) in
+        List.rev(word::cc_wrds)
+      else List.rev(cc_wrds)
+    end else
+    let c1 = String.get txt_in c in
+    match c1 with
+    | 'a'..'z' -> aux (c+1) (c1::cc_wrd) cc_wrds
+    | ' ' ->
+        let word = String.concat "" (List.map (String.make 1) (List.rev cc_wrd)) in
+        aux (c+1) [] (word::cc_wrds)
+    | _ -> aux (c+1) cc_wrd cc_wrds
+  in
+  aux 0 [] []
+
 let m () =
-  let s = "fr-en" in
-  let lng1, lng2 = split_langs s in
-  Printf.printf "# %s %s\n%!" lng1 lng2;
+  let txt_in = "said prince andrew gently" in
+  let sent = txt_to_sent txt_in in
+  List.iter (fun word ->
+    Printf.printf "#w:%s\n%!" word;
+  ) sent;
 ;;
 
 let () =
   let dict_file = "./test5.se" in
   let se = SExpr.parse_file dict_file in
-  let translate = ("fr", "en") in
-  let _ = translate in
+  let lng1, lng2 = split_langs "en-ru" in
+  Printf.printf "> %s %s\n%!" lng1 lng2;
+
+  let txt_in = input_file "./tr5.in" in
+  let sent = txt_to_sent txt_in in
+
+  (*
   let sent = "said"::"prince"::"andrew"::"gently"::[] in
-  let _ = sent in
+  *)
+
   let aux_word_2 se =
     match se with
     | SExpr.Expr ((SExpr.Atom lg)::(SExpr.Atom word_lg)::[]) -> Some(lg, word_lg)
@@ -46,7 +89,6 @@ let () =
       let _wrd = ref "" in
       let found = ref false in
       List.iter (fun (lg, wd) ->
-        (* Printf.printf "# %s %s\n%!" lg wd; *)
         if (lg, wd) = ("en", word) then found := true
       ) ass_word;
       if !found then begin
@@ -54,7 +96,7 @@ let () =
           if lg = "ru" then _wrd := wd
         ) ass_word;
       end;
-      if !_wrd <> "" then Printf.printf "# %s\n%!" !_wrd;
+      if !_wrd <> "" then Printf.printf "# %s\n%!" ( !_wrd);
     ) ass_words;
     Printf.printf "\n%!";
   ) sent;
